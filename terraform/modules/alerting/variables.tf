18248d2b0055c6c26b60a90520c92c2e7a7f00ae
localCoder1 <vighneshgpindoria@yahoo.co.uk>
2024-06-07 13:09:21 +0100

    Restore alerting modules

---
+++ terraform/modules/alerting/variables.tf
@@ -0,0 +1,19 @@
+variable "service_name" {
+  type        = string
+  description = "Name of the monitored service."
+}
+
+variable "service_url" {
+  type        = string
+  description = "URL of the monitored service."
+}
+
+variable "notification_channels" {
+  type        = set(string)
+  description = "GCP Notifications Channels that should receive the alerts."
+}
+
+variable "uptime_check_credentials" {
+  type        = object({ header_name = string, header_value = string })
+  description = "Credentials for the uptime checker."
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -178,23 +178,3 @@ variable "redis_memory_size" {
   description = "Redis memory size in GiB"
   default     = 1
 }
-
-variable "service_name" {
-  type        = string
-  description = "Name of the monitored service."
-}
-
-variable "service_url" {
-  type        = string
-  description = "URL of the monitored service."
-}
-
-variable "notification_channels" {
-  type        = set(string)
-  description = "GCP Notifications Channels that should receive the alerts."
-}
-
-variable "uptime_check_credentials" {
-  type        = object({ header_name = string, header_value = string })
-  description = "Credentials for the uptime checker."
-}
