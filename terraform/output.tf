d4621670b01688efdb88a55902a199121f102c8f
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-06-10 17:53:56 +0100

    Test terraform deployment of VPC and Cloud Run - as is from Beta
    Signed-off-by: localCoder1 <vighneshgpindoria@yahoo.co.uk>

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,10 @@
-output "load_balancer_ip" {
-  value = module.loadbalancer.external_ip
-}
+# output "load_balancer_ip" {
+#   value = module.loadbalancer.external_ip
+# }
 
-output "monitoring_dashboard_url" {
-  value = "https://console.cloud.google.com/monitoring/dashboards/builder/${split("/", google_monitoring_dashboard.dashboard.id)[3]}"
-}
+# output "monitoring_dashboard_url" {
+#   value = "https://console.cloud.google.com/monitoring/dashboards/builder/${split("/", google_monitoring_dashboard.dashboard.id)[3]}"
+# }
 
 output "publish_service_url" {
   description = "Link to the prototype publish service"
