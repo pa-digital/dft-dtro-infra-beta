d8d524e1bd1bc318e187ace0eafd1164e93adfa3
localCoder1 <vighneshgpindoria@yahoo.co.uk>
2024-06-17 17:41:06 +0100

    Test terraform deployment of VPC and Cloud Run - WiP 3
    Signed-off-by: localCoder1 <vighneshgpindoria@yahoo.co.uk>

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,43 +4,43 @@ locals {
 }
 
 #ALB VPC
-module "alb_vpc_network" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 9.1"
-
-  project_id   = var.project
-  network_name = "${local.name_prefix}-alb-network"
-
-  subnets = [
-    {
-      subnet_name   = "alb-subnet"
-      subnet_ip     = var.alb_vpc_ip_range
-      subnet_region = var.region
-    }
-  ]
-}
-
-#Backend VPC
-module "backend_vpc_network" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 9.1"
-
-  project_id   = var.project
-  network_name = "${local.name_prefix}-backend-network"
-
-  subnets = [
-    {
-      subnet_name   = "backend-subnet"
-      subnet_ip     = var.backend_vpc_ip_range
-      subnet_region = var.region
-    },
-    {
-      subnet_name   = local.serverless_subnet_name
-      subnet_ip     = var.backend_vpc_ip_range
-      subnet_region = var.region
-    }
-  ]
-}
+# module "alb_vpc_network" {
+#   source  = "terraform-google-modules/network/google"
+#   version = "~> 9.1"
+#
+#   project_id   = var.project
+#   network_name = "${local.name_prefix}-alb-network"
+#
+#   subnets = [
+#     {
+#       subnet_name   = "alb-subnet"
+#       subnet_ip     = var.alb_vpc_ip_range
+#       subnet_region = var.region
+#     }
+#   ]
+# }
+#
+# #Backend VPC
+# module "backend_vpc_network" {
+#   source  = "terraform-google-modules/network/google"
+#   version = "~> 9.1"
+#
+#   project_id   = var.project
+#   network_name = "${local.name_prefix}-backend-network"
+#
+#   subnets = [
+#     {
+#       subnet_name   = "backend-subnet"
+#       subnet_ip     = var.backend_vpc_ip_range
+#       subnet_region = var.region
+#     },
+#     {
+#       subnet_name   = local.serverless_subnet_name
+#       subnet_ip     = var.backend_vpc_ip_range
+#       subnet_region = var.region
+#     }
+#   ]
+# }
 
 #  VPC peering link to Cloud SQL VPC
 # module "cloudsql_private_service_access" {
@@ -54,20 +54,20 @@ module "backend_vpc_network" {
 # }
 
 ## This could be redundant, we can use Direct VPC egress to connect to the database VPC instead of this.
-resource "google_vpc_access_connector" "serverless_connector" {
-  name    = "${local.name_prefix}-connector"
-  project = var.project
-  region  = var.region
-
-  subnet {
-    project_id = var.project
-    name       = module.backend_vpc_network.subnets["${var.region}/${local.serverless_subnet_name}"].name
-  }
-
-  machine_type  = var.serverless_connector_config.machine_type
-  min_instances = var.serverless_connector_config.min_instances
-  max_instances = var.serverless_connector_config.max_instances
-}
+# resource "google_vpc_access_connector" "serverless_connector" {
+#   name    = "${local.name_prefix}-connector"
+#   project = var.project
+#   region  = var.region
+#
+#   subnet {
+#     project_id = var.project
+#     name       = module.backend_vpc_network.subnets["${var.region}/${local.serverless_subnet_name}"].name
+#   }
+#
+#   machine_type  = var.serverless_connector_config.machine_type
+#   min_instances = var.serverless_connector_config.min_instances
+#   max_instances = var.serverless_connector_config.max_instances
+# }
 
 resource "google_compute_region_network_endpoint_group" "publish_service_serverless_neg" {
   name                  = "${local.name_prefix}-${var.publish_service_image}-serverless-neg"
