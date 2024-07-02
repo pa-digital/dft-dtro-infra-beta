90d8d3a64685314e0b63c900ea846fd653f9998c
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-02 22:08:08 +0100

    Terraform deployment - Deploy to DfT GCP - test  tf backend-config
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/network.tf
+++ terraform/network.tf
@@ -87,121 +87,121 @@ resource "google_compute_region_network_endpoint_group" "publish_service_serverl
   }
 }
 
-## VPC Service Control
-# Manage access policy
-module "org_policy" {
-  count       = 0
-  source      = "terraform-google-modules/vpc-service-controls/google"
-  parent_id   = var.organisation_id
-  policy_name = "${local.network_name_prefix}-vpc-sc-policy"
-}
-
-module "access_level_members" {
-  count   = 0
-  source  = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
-  policy  = module.org_policy.policy_id
-  name    = "dtro_env_access_members"
-  members = ["serviceAccount:${var.cloud_run_service_account}", "serviceAccount:${var.wip_service_account}"] # List of members with access to the policy (service accounts)
-}
-
-#  According to the docs(https://github.com/terraform-google-modules/terraform-google-vpc-service-controls?tab=readme-ov-file#known-limitations),
-#  there may be a delay between a successful response and the change taking effect.
-resource "null_resource" "wait_for_members" {
-  count = 0
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-  depends_on = [module.access_level_members]
-}
-
-# Regular perimeter: Regular service perimeters protect services on the projects they contain.
-module "dtro_regular_service_perimeter" {
-  count               = 0
-  source              = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
-  policy              = module.org_policy.policy_id
-  perimeter_name      = "dtro_regular_service_perimeter"
-  description         = "Perimeter shielding DTRO project"
-  resources           = [var.project_id, "alb-vpc", "backend-vpc"]
-  access_levels       = [module.access_level_members.name]
-  restricted_services = ["Apigee API", "BigQuery API", "Artifact Registry API", "Compute Engine API", "Cloud Run Admin API", "Cloud SQL", "Cloud SQL Admin API", "Security Token Service API", "IAM Service Account Credentials API", "Cloud Source Repositories API", "Serverless VPC Access API", "Cloud Deployment Manager V2 API", "Cloud Logging API", "Service Networking API", "Cloud Resource Manager API", "Certificate Manager API", "Network Management API"]
-  shared_resources = {
-    all = ["${var.project_id}/${var.project_number}"]
-  }
-  ingress_policies = [
-    {
-      from = {
-        sources = [
-          {
-            access_level = "accessPolicies/${var.access_policy_id}/accessLevels/${var.access_level_name}"
-          }
-        ]
-      }
-      to = {
-        operations = [
-          {
-            service_name = "apigee.googleapis.com",
-            method_selectors = [
-              {
-                method = "*"
-              }
-            ]
-          }
-        ]
-      }
-    },
-    {
-      from = {
-        sources = [
-          {
-            service_account = "serviceAccount:${var.cloud_run_service_account}"
-          },
-          {
-            service_account = "serviceAccount:${var.wip_service_account}"
-          }
-        ]
-      }
-      to = {
-        operations = [
-          {
-            service_name = "run.googleapis.com",
-            method_selectors = [
-              {
-                method = "*"
-              }
-            ]
-          },
-          {
-            service_name = "sqladmin.googleapis.com",
-            method_selectors = [
-              {
-                method = "*"
-              }
-            ]
-          }
-        ]
-      }
-    }
-  ]
-
-  egress_policies = [
-    {
-      from = {
-        operations = [
-          {
-            service_name = "apigee.googleapis.com",
-            method_selectors = [
-              {
-                method = "*"
-              }
-            ]
-          }
-        ]
-      }
-      to = {
-        resources = [
-          "accessPolicies/${var.access_policy_id}/resources/ALB_RESOURCE"
-        ]
-      }
-    }
-  ]
-}
+# ## VPC Service Control
+# # Manage access policy
+# module "org_policy" {
+#   count       = 0
+#   source      = "terraform-google-modules/vpc-service-controls/google"
+#   parent_id   = var.organisation_id
+#   policy_name = "${local.network_name_prefix}-vpc-sc-policy"
+# }
+#
+# module "access_level_members" {
+#   count   = 0
+#   source  = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
+#   policy  = module.org_policy.policy_id
+#   name    = "dtro_env_access_members"
+#   members = ["serviceAccount:${var.cloud_run_service_account}", "serviceAccount:${var.wip_service_account}"] # List of members with access to the policy (service accounts)
+# }
+#
+# #  According to the docs(https://github.com/terraform-google-modules/terraform-google-vpc-service-controls?tab=readme-ov-file#known-limitations),
+# #  there may be a delay between a successful response and the change taking effect.
+# resource "null_resource" "wait_for_members" {
+#   count = 0
+#   provisioner "local-exec" {
+#     command = "sleep 60"
+#   }
+#   depends_on = [module.access_level_members]
+# }
+#
+# # Regular perimeter: Regular service perimeters protect services on the projects they contain.
+# module "dtro_regular_service_perimeter" {
+#   count               = 0
+#   source              = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
+#   policy              = module.org_policy.policy_id
+#   perimeter_name      = "dtro_regular_service_perimeter"
+#   description         = "Perimeter shielding DTRO project"
+#   resources           = [var.project_id, "alb-vpc", "backend-vpc"]
+#   access_levels       = [module.access_level_members.name]
+#   restricted_services = ["Apigee API", "BigQuery API", "Artifact Registry API", "Compute Engine API", "Cloud Run Admin API", "Cloud SQL", "Cloud SQL Admin API", "Security Token Service API", "IAM Service Account Credentials API", "Cloud Source Repositories API", "Serverless VPC Access API", "Cloud Deployment Manager V2 API", "Cloud Logging API", "Service Networking API", "Cloud Resource Manager API", "Certificate Manager API", "Network Management API"]
+#   shared_resources = {
+#     all = ["${var.project_id}/${var.project_number}"]
+#   }
+#   ingress_policies = [
+#     {
+#       from = {
+#         sources = [
+#           {
+#             access_level = "accessPolicies/${var.access_policy_id}/accessLevels/${var.access_level_name}"
+#           }
+#         ]
+#       }
+#       to = {
+#         operations = [
+#           {
+#             service_name = "apigee.googleapis.com",
+#             method_selectors = [
+#               {
+#                 method = "*"
+#               }
+#             ]
+#           }
+#         ]
+#       }
+#     },
+#     {
+#       from = {
+#         sources = [
+#           {
+#             service_account = "serviceAccount:${var.cloud_run_service_account}"
+#           },
+#           {
+#             service_account = "serviceAccount:${var.wip_service_account}"
+#           }
+#         ]
+#       }
+#       to = {
+#         operations = [
+#           {
+#             service_name = "run.googleapis.com",
+#             method_selectors = [
+#               {
+#                 method = "*"
+#               }
+#             ]
+#           },
+#           {
+#             service_name = "sqladmin.googleapis.com",
+#             method_selectors = [
+#               {
+#                 method = "*"
+#               }
+#             ]
+#           }
+#         ]
+#       }
+#     }
+#   ]
+#
+#   egress_policies = [
+#     {
+#       from = {
+#         operations = [
+#           {
+#             service_name = "apigee.googleapis.com",
+#             method_selectors = [
+#               {
+#                 method = "*"
+#               }
+#             ]
+#           }
+#         ]
+#       }
+#       to = {
+#         resources = [
+#           "accessPolicies/${var.access_policy_id}/resources/ALB_RESOURCE"
+#         ]
+#       }
+#     }
+#   ]
+# }
