5dbf0b45dabb1b945a6d958c840e2b08c13fbbe1
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-02 08:52:20 +0100

    Terraform deployment - Deploy to DfT GCP - rename Repository
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "artifact_repository" {
   location      = var.region
-  repository_id = "${var.project}-Repository"
+  repository_id = "${var.project}-repository"
   description   = "Repository for housing prototype images"
   format        = "DOCKER"
   docker_config {
