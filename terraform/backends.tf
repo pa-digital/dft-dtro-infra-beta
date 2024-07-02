b4bfa07c56fbb812df6c283cdcff4fc720380a83
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-02 22:06:18 +0100

    Terraform deployment - Deploy to DfT GCP - test  tf backend-config
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/backends.tf
+++ terraform/backends.tf
@@ -2,11 +2,11 @@ terraform {
   backend "gcs" {}
 }
 
-data "terraform_remote_state" "state" {
-  backend = "gcs"
-  config {
-    #     bucket = "dft-d-tro-terraform" # PA bucket
-    bucket = var.tf_state_bucket # DfT bucket
-    prefix = "terraform/state"
-  }
-}
+# data "terraform_remote_state" "state" {
+#   backend = "gcs"
+#   config = {
+#     #     bucket = "dft-d-tro-terraform" # PA bucket
+#     bucket = var.tf_state_bucket # DfT bucket
+#     prefix = "terraform/state"
+#   }
+# }
