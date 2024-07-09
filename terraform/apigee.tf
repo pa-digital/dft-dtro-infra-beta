5765783e9f149e9d7b5bd12e4b19325f6621a837
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-09 11:33:09 +0100

    Terraform deployment - Await terraform Deploy Apigee org and env until existing Apigee has been deleted and delete google_sql_ssl_cert
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/apigee.tf
+++ terraform/apigee.tf
@@ -1,48 +1,42 @@
-# Apigee
-resource "google_apigee_organization" "apigee_org" {
-  count              = 0
-  project_id         = local.project_id
-  analytics_region   = var.region
-  display_name       = "${local.name_prefix}-apigee-org"
-  description        = "Terraform-provisioned D-TRO Apigee Org."
-  runtime_type       = "CLOUD"
-  authorized_network = module.alb_vpc_network.network_id
-  retention          = "DELETION_RETENTION_UNSPECIFIED"
-  billing_type       = "PAYG"
-}
-
-resource "google_apigee_instance" "apigee_instance" {
-  count    = 0
-  name     = "${var.application_name}-apigee-instance"
-  location = var.region
-  org_id   = google_apigee_organization.apigee_org.id
-  ip_range = "10.9.0.0/22"
-}
-
-resource "google_apigee_environment" "apigee_env" {
-  count        = 0
-  org_id       = google_apigee_organization.apigee_org.id
-  name         = "${local.name_prefix}-apigee-environment"
-  description  = "${var.environment} ${var.application_name} Apigee Environment"
-  display_name = "${var.environment} ${var.application_name} Environment"
-  type         = "INTERMEDIATE"
-}
-
-resource "google_apigee_instance_attachment" "attachment" {
-  count       = 0
-  instance_id = google_apigee_instance.apigee_instance.id
-  environment = google_apigee_environment.apigee_env.name
-}
-
-resource "google_apigee_envgroup" "env_group" {
-  count     = 0
-  name      = "${local.name_prefix}-apigee-environment-group"
-  hostnames = var.environment == "prod" ? ["dtro.${var.org_domain}"] : ["${var.environment}.${var.org_domain}"]
-  org_id    = google_apigee_organization.apigee_org.id
-}
-
-resource "google_apigee_envgroup_attachment" "group_attachment" {
-  count       = 0
-  envgroup_id = google_apigee_envgroup.env_group.id
-  environment = google_apigee_environment.apigee_env.name
-}
+## Apigee
+# resource "google_apigee_organization" "apigee_org" {
+#   project_id         = local.project_id
+#   analytics_region   = var.region
+#   display_name       = "${local.name_prefix}-apigee-org"
+#   description        = "Terraform-provisioned D-TRO Apigee Org."
+#   runtime_type       = "CLOUD"
+#   authorized_network = module.alb_vpc_network.network_id
+#   retention          = "DELETION_RETENTION_UNSPECIFIED"
+#   billing_type       = "PAYG"
+# }
+#
+# resource "google_apigee_instance" "apigee_instance" {
+#   name     = "${var.application_name}-apigee-instance"
+#   location = var.region
+#   org_id   = google_apigee_organization.apigee_org.id
+#   ip_range = "10.9.0.0/22"
+# }
+#
+# resource "google_apigee_environment" "apigee_env" {
+#   org_id       = google_apigee_organization.apigee_org.id
+#   name         = "${local.name_prefix}-apigee-environment"
+#   description  = "${var.environment} ${var.application_name} Apigee Environment"
+#   display_name = "${var.environment} ${var.application_name} Environment"
+#   type         = "INTERMEDIATE"
+# }
+#
+# resource "google_apigee_instance_attachment" "attachment" {
+#   instance_id = google_apigee_instance.apigee_instance.id
+#   environment = google_apigee_environment.apigee_env.name
+# }
+#
+# resource "google_apigee_envgroup" "env_group" {
+#   name      = "${local.name_prefix}-apigee-environment-group"
+#   hostnames = var.environment == "prod" ? ["dtro.${var.org_domain}"] : ["${var.environment}.${var.org_domain}"]
+#   org_id    = google_apigee_organization.apigee_org.id
+# }
+#
+# resource "google_apigee_envgroup_attachment" "group_attachment" {
+#   envgroup_id = google_apigee_envgroup.env_group.id
+#   environment = google_apigee_environment.apigee_env.name
+# }
