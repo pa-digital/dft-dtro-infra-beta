aa8e5f6ceed3b0ec2bca79375ea5e239362e3cfe
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-18 10:50:39 +0100

    Terraform deployment - build-apigee-proxies-and-products - Deploy Proxy - #4
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- apigee/environments/dev.sh
+++ apigee/environments/dev.sh
@@ -4,3 +4,4 @@ echo "dev"
 echo "env=dev" >> $GITHUB_ENV
 echo "workload_identity_service_account=${service_account}" >> $GITHUB_ENV
 echo "apigee_organisation=${apigee_organisation}" >> $GITHUB_ENV
+echo "APIGEECLI_DEBUG=true" >> $GITHUB_ENV
