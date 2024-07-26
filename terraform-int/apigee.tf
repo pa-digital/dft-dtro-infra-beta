b75407164e05196f9bb71f113954386b6e3c6e11
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-26 20:22:28 +0100

    DPPB-420: apigee import - #3
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/apigee.tf
+++ terraform-int/apigee.tf
@@ -9,7 +9,7 @@ resource "google_apigee_instance" "apigee_instance" {
 }
 
 resource "google_apigee_environment" "apigee_env" {
-  org_id       = data.google_apigee_organization.apigee_org.id
+  org_id       = google_apigee_organization.apigee_org.id
   name         = "${local.name_prefix}-apigee-environment"
   description  = "${var.environment} ${var.application_name} Apigee Environment"
   display_name = "${local.name_prefix} Environment"
@@ -18,14 +18,14 @@ resource "google_apigee_environment" "apigee_env" {
 }
 
 resource "google_apigee_instance_attachment" "attachment" {
-  instance_id = data.google_apigee_instance.apigee_instance.id
+  instance_id = google_apigee_instance.apigee_instance.id
   environment = google_apigee_environment.apigee_env.name
 }
 
 resource "google_apigee_envgroup" "env_group" {
   name      = "${local.name_prefix}-apigee-environment-group"
   hostnames = [var.domain[var.environment]]
-  org_id    = data.google_apigee_organization.apigee_org.id
+  org_id    = google_apigee_organization.apigee_org.id
 }
 
 resource "google_apigee_envgroup_attachment" "group_attachment" {
