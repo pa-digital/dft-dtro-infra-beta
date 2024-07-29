316a15677be477fac9819420d6812107aba105d0
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-29 16:32:17 +0100

    DPPB-420: Terraform plan -
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/data.tf
+++ terraform-int/data.tf
@@ -4,3 +4,7 @@ data "google_project" "project" {}
 data "google_organization" "organisation" {
   domain = var.org_domain
 }
+
+data "google_compute_network" "alb_vpc_network" {
+  name = "${var.application_name}-${var.environment}-alb-network"
+}
