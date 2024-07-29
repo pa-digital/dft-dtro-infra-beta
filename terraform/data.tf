c71eb487292654d8145ddd1da1e511a3ef36c6f8
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-29 10:01:22 +0100

    terraform output
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,10 @@
 data "google_project" "project" {}
 
 # Commented out until we have valid organisation domain from DfT
-# data "google_organization" "organisation" {
-#   domain = var.org_domain
-# }
+data "google_organization" "organisation" {
+  domain = var.org_domain
+}
+
+output "apigee_instance" {
+  value = google_apigee_instance.apigee_instance.id
+}
