093526440369c0855dec88d91054b79b491f9b28
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-31 21:46:33 +0100

    Terraform deployment - int
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- apigee/environments/int.sh
+++ apigee/environments/int.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "int"
-echo "env=dev" >> $GITHUB_ENV
+echo "env=test" >> $GITHUB_ENV
 echo "env_name_prefix=int" >> $GITHUB_ENV
 echo "workload_identity_service_account=${service_account}" >> $GITHUB_ENV
 echo "apigee_organisation=${apigee_organisation}" >> $GITHUB_ENV
