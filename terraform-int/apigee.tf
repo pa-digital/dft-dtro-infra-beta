c3fdc40d3ccd8b1ac6f7aff0b05d96b51c01c2ea
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-07-31 14:43:40 +0100

    Terraform deployment - Test deployment to test - apply
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/apigee.tf
+++ terraform-int/apigee.tf
@@ -1,20 +1,3 @@
-# resource "google_apigee_organization" "apigee_org" {
-#   project_id         = local.project_id
-#   analytics_region   = var.region
-#   display_name       = "${var.application_name}-${var.environment}-apigee-org"
-#   description        = "Terraform-provisioned D-TRO Apigee Org."
-#   runtime_type       = "CLOUD"
-#   authorized_network = data.google_compute_network.alb_vpc_network.id
-#   retention          = "DELETION_RETENTION_UNSPECIFIED"
-#   billing_type       = "PAYG"
-# }
-#
-# resource "google_apigee_instance" "apigee_instance" {
-#   name     = "${var.application_name}-apigee-instance"
-#   location = var.region
-#   org_id   = google_apigee_organization.apigee_org.id
-# }
-
 resource "google_apigee_environment" "apigee_env" {
   org_id       = data.terraform_remote_state.primary_default_tfstate.outputs.apigee_org
   name         = "${local.name_prefix}-apigee-environment"
