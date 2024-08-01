47596f30483b68e1af159c2e396697d2ad040e5d
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-01 11:51:52 +0100

    DPPB-488: update to int
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/locals.tf
+++ terraform-int/locals.tf
@@ -1,6 +1,5 @@
 locals {
   apigee-mig-proxy       = "apigee-mig-proxy"
-  artifact_registry_name = "${data.google_project.project.name}-int-repository"
   cloud_run_service_name = "${local.name_prefix}-${var.dtro_service_image}"
   database_name          = "${local.name_prefix}-database"
   database_username      = var.application_name
--- terraform-int/registry.tf
+++ terraform-int/registry.tf
@@ -1,7 +1,17 @@
-resource "google_artifact_registry_repository" "artifact_repository" {
+resource "google_artifact_registry_repository" "dtro_artifact_repository" {
   location      = var.region
-  repository_id = local.artifact_registry_name
+  repository_id = "${data.google_project.project.name}-int-repository"
   description   = "Repository for housing prototype images"
+  format        = "DOCKER"
+  docker_config {
+    immutable_tags = false
+  }
+}
+
+resource "google_artifact_registry_repository" "service_ui_artifact_repository" {
+  location      = var.region
+  repository_id = "${data.google_project.project.name}-ui-int-repository"
+  description   = "Repository for housing Service Portal UI images"
   format        = "DOCKER"
   docker_config {
     immutable_tags = false
