068e3529f1e6eda6579ecb5f63716a9cd7f57918
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-12 16:10:50 +0100

    DPPB-420: Terraform Apply - Correct int alb set up - #5
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -116,7 +116,6 @@ resource "google_compute_instance_template" "apigee_mig" {
 }
 
 resource "google_compute_region_instance_group_manager" "apigee_mig" {
-  count = 0
   project            = local.project_id
   name               = "${local.apigee-mig}-proxy"
   region             = var.region
