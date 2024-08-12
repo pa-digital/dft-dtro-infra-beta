52f15f158a1cf2959a74c70d2d19195ca676e84b
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-12 14:42:06 +0100

    DPPB-420: Terraform Apply - Correct name of access connector #2
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/lb.tf
+++ terraform-int/lb.tf
@@ -484,8 +484,8 @@ resource "google_compute_firewall" "health_check_firewall_rule" {
 }
 
 # Endpoint attachment in the Cloud Run CSO Service UI project
-resource "google_vpc_access_connector" "ui_vpc_connector" {
-  name   = "${local.name_prefix}-ui-cloud-run-connector"
+resource "google_vpc_access_connector" "int_ui_vpc_connector" {
+  name   = "${local.name_prefix}-cloud-run-connector"
   region = var.region
   subnet {
     project_id = data.google_project.project.project_id
