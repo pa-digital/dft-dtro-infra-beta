498bd85068cd2803c3e8fc2ff0f715a662ac96ef
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-12 12:27:14 +0100

    DPPB-420: Terraform Plan - Int set up - #3
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/data.tf
+++ terraform-int/data.tf
@@ -1,9 +1,8 @@
 data "google_project" "project" {}
 
-# Commented out until we have valid organisation domain from DfT
-data "google_organization" "organisation" {
-  domain = var.org_domain
-}
+# data "google_organization" "organisation" {
+#   domain = var.org_domain
+# }
 
 data "google_compute_network" "alb_vpc_network" {
   name = "${var.application_name}-${var.environment}-alb-network"
