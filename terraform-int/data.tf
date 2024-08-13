9a28cf2e6ff7105c586c7de9a3dfa8546642add4
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-13 12:03:59 +0100

    DPPB-420: Remove int-apigee-mig, int-xlb to use test-apigee-mig - Use test-apigee-mig outputs
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/data.tf
+++ terraform-int/data.tf
@@ -1,9 +1,5 @@
 data "google_project" "project" {}
 
-# data "google_organization" "organisation" {
-#   domain = var.org_domain
-# }
-
 data "google_compute_network" "alb_vpc_network" {
   name = "${var.application_name}-${var.environment}-alb-network"
 }
