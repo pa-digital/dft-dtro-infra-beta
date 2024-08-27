c03ce1471f24ad3e779ec7e9209d9bf0b9250db5
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-08-27 11:20:57 +0100

    DPPB-488: Delete UI ilb and mig - int - #3
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform-int/lb.tf
+++ terraform-int/lb.tf
@@ -317,22 +317,6 @@ resource "google_compute_subnetwork" "ui_ilb_subnetwork" {
   purpose       = "PRIVATE"
 }
 
-# Create a backend service for each Cloud Run service
-resource "google_compute_region_backend_service" "apigee_backend_service" {
-  project               = local.project_id
-  name                  = "${local.int-apigee-mig}-backend-service"
-  region                = var.region
-  load_balancing_scheme = "INTERNAL_MANAGED"
-  protocol              = "HTTP"
-  health_checks         = [google_compute_region_health_check.ui_ilb_health_check.id]
-  backend {
-    group           = google_compute_region_instance_group_manager.ui_apigee_mig.instance_group
-    balancing_mode  = "UTILIZATION"
-    capacity_scaler = 1.0
-    max_utilization = var.cpu_max_utilization
-  }
-}
-
 resource "google_compute_region_health_check" "ui_ilb_health_check" {
   project = local.project_id
   name    = "${local.name_prefix}-ui-ilb-health-check"
