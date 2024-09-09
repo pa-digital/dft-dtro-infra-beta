93030e4a2051c5d71c6c32d7e39fae6389126740
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-09-09 20:55:59 +0100

    DPPB-558: comment out use of "null_resource" "wait_for_members"
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/network.tf
+++ terraform/network.tf
@@ -124,7 +124,7 @@ module "access_level_members" {
 #  According to the docs(https://github.com/terraform-google-modules/terraform-google-vpc-service-controls?tab=readme-ov-file#known-limitations),
 #  there may be a delay between a successful response and the change taking effect.
 resource "null_resource" "wait_for_members" {
-  #   count = 0
+  count = 0
   provisioner "local-exec" {
     command = "sleep 60"
   }
@@ -138,7 +138,7 @@ module "dtro_regular_service_perimeter" {
   version        = "6.0.0"
   policy         = module.org_policy[0].policy_id
   perimeter_name = "${local.name_prefix}-regular-service-perimeter"
-  description    = "Perimeter shielding DTRO project ${null_resource.wait_for_members.id}"
+  description    = "Perimeter shielding DTRO project ${null_resource.wait_for_members.id}" #"Perimeter shielding DTRO project ${null_resource.wait_for_members.id}"
   #   resources           = [data.google_project.project.number]
   #   access_levels       = [module.access_level_members[0].name]
   #   restricted_services = ["apigee.googleapis.com", "artifactregistry.googleapis.com", "bigquery.googleapis.com", "certificatemanager.googleapis.com", "cloudbuild.googleapis.com", "clouddeploy.googleapis.com", "cloudresourcemanager.googleapis.com", "compute.googleapis.com", "iap.googleapis.com", "iam.googleapis.com", "iamcredentials.googleapis.com", "logging.googleapis.com", "networkmanagement.googleapis.com", "osconfig.googleapis.com", "run.googleapis.com", "secretmanager.googleapis.com", "servicenetworking.googleapis.com", "sourcerepo.googleapis.com", "sql-component.googleapis.com", "sqladmin.googleapis.com", "sts.googleapis.com", "storage-component.googleapis.com", "storage.googleapis.com", "websecurityscanner.googleapis.com", "vpcaccess.googleapis.com"]
