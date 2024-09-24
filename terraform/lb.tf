77aeff96b4b63b386df34a607378e31ff373ddcb
VinP-PA <vighnesh.pindoria@paconsulting.com>
2024-09-24 10:20:40 +0100

    DPPB-537: Checkov check
    Signed-off-by: VinP-PA <vighnesh.pindoria@paconsulting.com>

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -35,7 +35,7 @@ module "loadbalancer" {
 
       groups = [
         {
-          group           = google_compute_region_instance_group_manager.apigee_mig2.instance_group
+          group           = google_compute_region_instance_group_manager.apigee_mig.instance_group
           max_utilization = var.cpu_max_utilization
         }
       ]
@@ -132,21 +132,21 @@ resource "google_compute_region_instance_group_manager" "apigee_mig" {
   }
 }
 
-# resource "google_compute_region_autoscaler" "apigee_autoscaler" {
-#   project = local.project_id
-#   name    = "${local.apigee-mig}-autoscaler"
-#   region  = var.region
-#   target  = google_compute_region_instance_group_manager.apigee_mig.id
-#   # TODO: Assess if these values are sufficient or requires updating
-#   autoscaling_policy {
-#     max_replicas    = 3
-#     min_replicas    = 2
-#     cooldown_period = 90
-#     cpu_utilization {
-#       target = var.cpu_max_utilization
-#     }
-#   }
-# }
+resource "google_compute_region_autoscaler" "apigee_autoscaler" {
+  project = local.project_id
+  name    = "${local.apigee-mig}-autoscaler"
+  region  = var.region
+  target  = google_compute_region_instance_group_manager.apigee_mig.id
+  # TODO: Assess if these values are sufficient or requires updating
+  autoscaling_policy {
+    max_replicas    = 3
+    min_replicas    = 2
+    cooldown_period = 90
+    cpu_utilization {
+      target = var.cpu_max_utilization
+    }
+  }
+}
 #########################################
 
 resource "google_compute_instance_template" "apigee_mig2" {
